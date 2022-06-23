6d7014e97bb70a75240cfa30acbc5ab6b4aea0bf
NicoloYan <nico.yan@outlook.it>
2022-06-23 15:43:07 +0200

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // BONUS:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue (
+
+    {
+        el: '#root',
+        data: {
+            title: 'Hello Vue',
+            picture: 'img/landscape.jpg'
+        }
+    }
+
+);
